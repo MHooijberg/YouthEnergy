321ea33109df1664d6d3a3339da28fed410be12f
JelleNHL <jlm05@hotmail.com>
2022-03-15 11:49:01 +0100

    Different changes
    Verbruiksmeter page is added and the footer has been changed to a better version. In nav bar is a duplicate removed

--- partials/navbarIndex.php
+++ partials/navbarIndex.php
@@ -6,7 +6,8 @@
         <img src="/images/logo.png" width="30" height="30" alt="">
     </a>
     <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="/index.php" class="nav-link">Home</a></li>
         <li class="nav-item"><a href="/intranet" class="nav-link">Intranet</a></li>
+        <li class="nav-item"><a href="/verbruiksmeter" class="nav-link">Verbruiksmeter</a></li>
     </ul>
 </nav>

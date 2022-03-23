b4bb5d48883f053e86ec963e90576889f20b15a2
TjerkVenema <tjerk.venema@student.nhlstenden.com>
2022-03-23 13:20:21 +0100

    klantenservice update

--- intranet/KlantenService.php
+++ intranet/KlantenService.php
@@ -19,17 +19,18 @@ if (isset($_POST['requestPH'])) {
     $user = 'website';       ///< the username to connect to the database
     $pass = 'wachtwoord';    ///< the password to connect to the database
     $connection = new PDO('mysql:host=localhost;dbname=energy', $user, $pass); ///< make the connection
-
     $KLANTENSERVICE_READ_METERSTANDEN =
-        "SELECT ms_stand, mt_type 
-        FROM tbl_meters_standen 
-        JOIN tbl_meter_telwerken 
-            on ms_fk_idMeterTelwerk = mt_idMeterTelwerk
-        JOIN tbl_meters 
-            on mt_fk_idMeter = m_idMeter
-        JOIN tbl_adressen 
-            on m_fk_idAdres = a_idAdres
-        WHERE a_postcode = :postcode AND a_huisnummer = :huisnummer";
+        "SELECT ms_stand,m_product
+    FROM tbl_meters_standen
+    JOIN tbl_meter_telwerken
+        on ms_fk_idMeterTelwerk = mt_idMeterTelwerk
+    JOIN tbl_meters
+        on mt_fk_idMeter = m_idMeter
+    JOIN tbl_adressen 
+        on m_fk_idAdres = a_idAdres
+        WHERE a_postcode = :postcode 
+            AND a_huisnummer = :huisnummer
+            AND mt_type='v'";
 
     $statement = $connection->prepare($KLANTENSERVICE_READ_METERSTANDEN);
     $statement->bindValue(':postcode', $postcode);
@@ -38,19 +39,34 @@ if (isset($_POST['requestPH'])) {
     $meterstanden = $statement->fetchAll();
 
     if (!empty($meterstanden)){
-        //$_SESSION["meterstanden"] = $meterstanden;
-
         $_SESSION['meter'] = array();
-        $_SESSION["meter"]["type"] = $meterstanden[0]["mt_type"];
-        $metercount = count($meterstanden);
-        $_SESSION["meter"]["count"] = $metercount;
-        $ms_total = 0;
+        $g_total = 0;
+        $e_total = 0;
+        $g_count = 0;
+        $e_count = 0;
+        $ms_mid_e = 0;
+        $ms_mid_g = 0;
         foreach ($meterstanden as $value){
-            $ms_total += $value["ms_stand"];
+            if ($value["m_product"] == 'G'){
+                $g_total += $value["ms_stand"];
+                $g_count++;
+            }
+            else if ($value["m_product"] == 'E'){
+                $e_total += $value["ms_stand"];
+                $e_count++;
+            }
         }
-        $ms_mid = intval($ms_mid = $ms_total / $metercount);
-        $_SESSION["meter"]["gemideld"] = $ms_mid;
+        if ($e_count != 0){
+            $ms_mid_e = intval($ms_mid = $e_total / $e_count);
+        }
+        if ($g_count != 0){
+            $ms_mid_g = intval($ms_mid = $g_total / $g_count);
+        }
 
+        $_SESSION["meter"]["e_gemideld"] = $ms_mid_e;
+        $_SESSION["meter"]["e_count"] = $e_count;
+        $_SESSION["meter"]["g_gemideld"] = $ms_mid_g;
+        $_SESSION["meter"]["g_count"] = $g_count;
         $_SESSION["HasClient"] = 1;
     }
 
@@ -81,7 +97,84 @@ if (isset($_POST['requestPH'])) {
 
 if (isset($_POST['requestKN'])) {
     $klantnummer = $_POST['Klantnummer'];
-    $user = 'website';       ///< the username to connect to the database
+$user = 'website';       ///< the username to connect to the database
+$pass = 'wachtwoord';    ///< the password to connect to the database
+$connection = new PDO('mysql:host=localhost;dbname=energy', $user, $pass); ///< make the connection
+$KLANTENSERVICE_READ_METERSTANDEN =
+    "SELECT ms_stand,m_product
+    FROM tbl_meters_standen
+    JOIN tbl_meter_telwerken
+        on ms_fk_idMeterTelwerk = mt_idMeterTelwerk
+    JOIN tbl_meters
+        on mt_fk_idMeter = m_idMeter
+    JOIN tbl_adressen 
+        on m_fk_idAdres = a_idAdres
+    JOIN tbl_klanten
+        on  k_fk_idAdres = a_idAdres
+    WHERE k_klantnummer = :klantnummer
+    AND mt_type='v'";
+
+
+$statement = $connection->prepare($KLANTENSERVICE_READ_METERSTANDEN);
+$statement->bindValue(':klantnummer', $klantnummer);
+$statement->execute();
+$meterstanden = $statement->fetchAll();
+
+if (!empty($meterstanden)){
+    $_SESSION['meter'] = array();
+    $g_total = 0;
+    $e_total = 0;
+    $g_count = 0;
+    $e_count = 0;
+    $ms_mid_e = 0;
+    $ms_mid_g = 0;
+    foreach ($meterstanden as $value){
+        if ($value["m_product"] == 'G'){
+            $g_total += $value["ms_stand"];
+            $g_count++;
+        }
+        else if ($value["m_product"] == 'E'){
+            $e_total += $value["ms_stand"];
+            $e_count++;
+        }
+    }
+    if ($e_count != 0){
+        $ms_mid_e = intval($ms_mid = $e_total / $e_count);
+    }
+    if ($g_count != 0){
+        $ms_mid_g = intval($ms_mid = $g_total / $g_count);
+    }
+
+    $_SESSION["meter"]["e_gemideld"] = $ms_mid_e;
+    $_SESSION["meter"]["e_count"] = $e_count;
+    $_SESSION["meter"]["g_gemideld"] = $ms_mid_g;
+    $_SESSION["meter"]["g_count"] = $g_count;
+    $_SESSION["HasClient"] = 1;
+}
+    $KLANTENSERVICE_READ_KLANTGEGEVENS = "
+        SELECT k_achternaam, k_voornaam, k_klantnummer, a_postcode, a_huisnummer, a_straatnaam
+        FROM tbl_klanten
+        JOIN tbl_adressen 
+            on k_fk_idAdres = a_idAdres
+        WHERE k_klantnummer = :klantnummer";
+
+    $statement = $connection->prepare($KLANTENSERVICE_READ_KLANTGEGEVENS);
+    $statement->bindValue(':klantnummer', $klantnummer);
+    $statement->execute();
+    $user = $statement->fetch();
+
+    if (!empty($meterstanden)) {
+        $_SESSION["user"]["voornaam"] = $user["k_voornaam"];
+        $_SESSION["user"]["achternaam"] = $user["k_achternaam"];
+        $_SESSION["user"]["klantnummer"] = $user["k_klantnummer"];
+        $_SESSION["user"]["postcode"] = $user["a_postcode"];
+        $_SESSION["user"]["huisnummer"] = $user["a_huisnummer"];
+        $_SESSION["user"]["straatnaam"] = $user["a_straatnaam"];
+    }
+    $connection = null;
+    $statement = null;
+}
+/*    $user = 'website';       ///< the username to connect to the database
     $pass = 'wachtwoord';    ///< the password to connect to the database
     $connection = new PDO('mysql:host=localhost;dbname=energy', $user, $pass); ///< make the connection
 
@@ -140,7 +233,7 @@ if (isset($_POST['requestKN'])) {
     }
     $connection = null;
     $statement = null;
-}
+}*/
 ?>
 <!doctype html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
@@ -213,9 +306,10 @@ if (isset($_POST['requestKN'])) {
                         <div class="row">
                             <div class="border">
                                 <h5>Meetgegevens van de klant</h5>
-                                <p>Type meter: <?=$_SESSION["meter"]["type"] ?></p>
-                                <p>Totaal aantal metingen: <?=$_SESSION["meter"]["count"] ?></p>
-                                <p>Gemideld verbruik: <?=$_SESSION["meter"]["gemideld"] ?></p>
+                                <p>Totaal aantal energy metingen: <?=$_SESSION["meter"]["e_count"] ?></p>
+                                <p>Gemideld energy verbruik: <?=$_SESSION["meter"]["e_gemideld"] ?></p>
+                                <p>Totaal aantal gas metingen: <?=$_SESSION["meter"]["g_count"] ?></p>
+                                <p>Gemideld gas verbruik: <?=$_SESSION["meter"]["g_gemideld"] ?></p>
                             </div>
                         </div>
 

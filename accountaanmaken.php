48d02b09ba26ef6292dfb63c749cbf9d63e48ef2
Psychopath24 <m.hooijberg24@gmail.com>
2022-03-25 12:54:31 +0100

    Sessie Vullen

--- accountaanmaken.php
+++ accountaanmaken.php
@@ -10,12 +10,16 @@
         <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-    <form>
+    <form id="AccountKoppelen">
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
                     <label for="InputKlantNummer">Klantnummer</label>
                     <input type="text" class="form-control" id="InputKlantNummer" placeholder="Klantnummer">
+                </div>
+                <div class="form-group">
+                    <label for="InputPostcode">Postcode</label>
+                    <input type="text" class="form-control" id="InputPostcode" placeholder="Postcode">
                 </div>
                 <div class="form-group">
                     <label for="InputEmail">Email address</label>
@@ -52,9 +56,31 @@
                     <input type="checkbox" class="form-check-input" id="CodeCheck">
                     <label class="form-check-label" for="CodeCheck">Ik geef toestemming dat Youth Energy mij een code opstuurd.</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <!-- Trigger the modal with a button -->
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
             </div>
         </div>
     </form>
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Modal Header</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Some text in the modal.</p>
+                </div>
+                <div class="modal-footer">
+                    <button form="AccountKoppelen" type="submit" class="btn btn-primary">Submit</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
     </body>
 </html>
